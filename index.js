30a98a8061ddd93e3678700f4be679d3de729a2a
Mark Voltaire <markjvoltaire@gmail.com>
2021-12-07 13:29:46 -0500

    filter added

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ export const filterOceania = (countries) => {
 // OUTPUT: only countries that include the letters 'au'
 // REQS: use .filter
 
-export const filterAu = (countries) => {};
+export const filterAu = (countries) => {
+  return countries.filter((country) => country.name.toLowerCase().includes('au'));
+};
 
 // INPUT: the array of brands from data.js
 // OUTPUT: an array of all the sneakers brands
